563a516abcba292fbf3ea2800e37ed237c6d2890
FION <uxfion@gmail.com>
2021-06-25 15:20:58 +0800

    尝试使用freeRTOS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# esp32-devkitc
+# ESP32-DevKitc 学习工程
+
+这是我学习ESP32的工程与记录
+希望在毕业设计上能用上ESP32这款模组
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ U8G2_SSD1306_128X64_NONAME_F_4W_SW_SPI u8g2(U8G2_R2, 32, 33, 27, 26,25);
 void setup(void) {
 //    Serial.begin(115200);
 
-    pinMode(14, OUTPUT);
+//    pinMode(14, OUTPUT);
 
     u8g2.begin();
     u8g2.enableUTF8Print();
@@ -19,10 +19,10 @@ void loop(void) {
 //    Serial.print("FUCK\n");
     vol += 0.1;
 
-    digitalWrite(14,HIGH);
-    delay(50);
-    digitalWrite(14,LOW);
-    delay(30);
+//    digitalWrite(14,HIGH);
+//    delay(50);
+//    digitalWrite(14,LOW);
+//    delay(30);
 
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_pcsenior_8f);
